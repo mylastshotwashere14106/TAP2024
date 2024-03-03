506486cbd1c14bf96f3464fd85331efd831410b3
angel <mylastshotwashere14106@protonmail.com>
2024-03-03 14:29:58 -0600

    Added the last stuff

--- src/main/java/com/example/tap2024/vistas/CuadroMagico.java
+++ src/main/java/com/example/tap2024/vistas/CuadroMagico.java
@@ -1,9 +1,11 @@
 package com.example.tap2024.vistas;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -24,9 +26,10 @@ public class CuadroMagico extends Stage {
     private HBox createLayout;
 
     public CuadroMagico(){
+        this.getIcons().add(new Image("https://i.4cdn.org/w/1709038154516331.png"));
         CrearUI();
         layout = new VBox(magicSquare,create,size);
-        Scene scene = new Scene(layout);
+        Scene scene = new Scene(layout,500,500);
         scene.getStylesheets().add(getClass().getResource("/estilos/cuadromagico.css").toString());
         this.setTitle("Cuadro Mágico");
         this.setScene(scene);
@@ -35,9 +38,12 @@ public class CuadroMagico extends Stage {
 
     private void CrearUI(){
         magicSquare = new GridPane();
+        magicSquare.setPrefSize(500,500);
+        magicSquare.setAlignment(Pos.CENTER);
         size = new TextField("3");
+        size.setPrefSize(500,25);
         create = new Button("Crear");
-        create.setPrefSize(100,25);
+        create.setPrefSize(500,25);
         create.setOnAction(event -> {
             if(!CheckInput.checkInt(size.getText())){
                 size.setText("Type a number you dummy");
